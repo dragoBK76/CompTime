09c12577ef6309ea1c623aab4c8de7e66c91805e
Josh Salez <josh.salez@gmail.com>
2016-01-31 14:45:26 -0500

    Nav Changes

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,7 +1,31 @@
 class ActivitiesController < ApplicationController
 
   def index
-    @user = User.find(params[:id])
+    @user = current_user
     @activities = Activity.all
   end
+
+  def new
+    @user = User.find(params[:id])
+    @activity = @user.activities.new
+  end
+
+  def create
+    @user = User.find(params[:id])
+    @activity = @user.activities.new(activity_params)
+
+    if @activity.save
+      redirect_to user_activities_path(@user.id)
+
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def activity_params
+    params.require(:activity).permit(:name, :date, :hours_worked, :notes)
+  end
+
 end
