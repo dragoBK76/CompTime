f0ac31b20b83800ebf8f5f2422ad425701bb12f7
Josh Salez <josh.salez@gmail.com>
2016-02-21 16:15:50 -0500

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-  user = User.first
-  user.admin = true
-  user.save
+  # user = User.first
+  # user.admin = true
+  # user.save
