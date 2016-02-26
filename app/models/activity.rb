0305a22f5c0759e642b7204ed9ecafdc51860850
DragoBK76 <josh.salez@gmail.com>
2016-02-26 11:33:42 -0500

    added scopes for approved hours and calculate accrued comp days

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,14 @@
 class Activity < ActiveRecord::Base
   belongs_to :user
+
+  scope :approved_hours, -> { where(approved: true).sum(:hours_worked) }
+  scope :accrued_days, -> { where(approved: true).sum(:hours_worked) / 8}
+
+  def self.calc_accrued_days
+    if (where(approved: true).sum(:hours_worked) % 8) > 3
+      (where(approved: true).sum(:hours_worked) / 8).to_i + 0.5
+    else
+      (where(approved: true).sum(:hours_worked) / 8).to_i
+    end
+  end
 end
