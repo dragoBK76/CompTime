0b1c1d7db53a4b1d50183d9686389aed29795e9d
Josh Salez <josh.salez@gmail.com>
2016-02-27 14:32:49 -0500

    update adds year variable

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,11 @@
 class AdminController < ApplicationController
 
   def index
+    if !current_user.admin?
+      redirect_to user_activities_path(current_user.id)
+    else
     @users = User.all
-    # @activities = Activity.all
+    @this_year = Date.current.year
+  end
   end
 end
