cd0993db908e257f5e1831b81818ea144739a409
Josh Salez <josh.salez@gmail.com>
2016-02-28 15:09:23 -0500

    moved new_activity_mailer to activity_mailer to test

--- app/mailers/activity_mailer.rb
+++ app/mailers/activity_mailer.rb
@@ -1,2 +1,11 @@
 class ActivityMailer < ApplicationMailer
+
+  def new_activity_alert(user)
+    @user = user
+    @url  = "https://arcomptime.herokuapp.com"
+    mail(:to => "kdjs@audiencerewards.com",
+         :subject => "#{user.fullname} created a new Comp-Time activity for review.")
+    
+  end
+  
 end
