b57e291370e65e5aeed1a8cd6098203eb71ab0c7
Josh Salez <josh.salez@gmail.com>
2016-06-29 21:07:56 -0400

    update activity model to correct partial days

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -6,7 +6,7 @@ class Activity < ActiveRecord::Base
   scope :accrued_days, -> { where(approved: true).sum(:hours_worked) / 8}
 
   def self.calc_accrued_days
-    if (where(approved: true).sum(:hours_worked) % 8) > 3
+    if (where(approved: true).sum(:hours_worked) % 8) >= 4
       (where(approved: true).sum(:hours_worked) / 8).to_i + 0.5
     else
       (where(approved: true).sum(:hours_worked) / 8).to_i
